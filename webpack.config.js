d3ff9bd6e9f00aff8a51f883718821ecab9a558b
Michael Vines <mvines@gmail.com>
2018-08-09 20:09:04 -0700

    Boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+/* eslint import/no-commonjs:0 */
+const webpack = require('webpack');
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  }
+};
