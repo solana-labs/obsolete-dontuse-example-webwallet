68c196e9709008d37dbdadcb8d4b5a294be954ab
Michael Vines <mvines@gmail.com>
2018-08-10 23:36:46 -0700

    UI 0.1

---
+++ src/web3-sol.js
@@ -0,0 +1,43 @@
+/*
+ * Solana Web3 Client Interface
+ *
+ */
+
+function sleep(duration: number = 0): Promise<void> {
+  return new Promise((accept) => {
+    setTimeout(accept, duration);
+  });
+}
+
+export class Web3Sol {
+  balance = 0;
+  publicKey = '';
+  endpoint = null;
+
+  constructor(endpoint) {
+    this.endpoint = endpoint;
+  }
+
+  async getPublicKey() {
+    await sleep(500); // TODO
+    this._publicKey = 'EJeyhbqwYBvoAbC1pfxiHJRrV5CZu3MZzP8kBu3sKthY'; // TODO
+
+    return this._publicKey;
+  }
+
+  async getBalance() {
+    await sleep(500); // TODO
+    return this._balance;
+  }
+
+  async requestAirdrop(amount) {
+    await sleep(500); // TODO
+    this._balance += amount;
+  }
+
+  async sendTokens(to, amount) {
+    await sleep(500); // TODO
+    throw new Error(`Unable to send ${amount} tokens to ${to}`);
+  }
+}
+
