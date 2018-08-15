16af81117eca9187e29f967f415f6e86a5a26b4a
Michael Vines <mvines@gmail.com>
2018-08-15 08:47:12 -0700

    Fix default path

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-// the __dirname is the current directory from where the script is running
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// send the user to index html page inspite of the url
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'dist/index.html'));
 });
 
 app.listen(port);
