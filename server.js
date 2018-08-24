67c0920205517e1cbc995415cf73921b941510f7
Michael Vines <mvines@gmail.com>
2018-08-23 20:52:27 -0700

    Display port

--- server.js
+++ server.js
@@ -9,4 +9,5 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist/index.html'));
 });
+console.log(`Listening on port ${port}`);
 app.listen(port);
